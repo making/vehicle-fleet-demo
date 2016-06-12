f4b0e8fb3250976b78c07b9c810f6362ffc88f80
making <makingx@gmail.com>
2016-06-12 20:02:49 +0900

    Support PCFDev

--- insert-data.sh
+++ insert-data.sh
@@ -8,8 +8,22 @@ if ! [ -e "serviceLocations.json" ]; then
 	wget http://assets.springone2gx2015.s3.amazonaws.com/fleet/serviceLocations.json
 fi
 
-#curl -H "Content-Type: application/json" localhost:9000/fleet -d @fleet.json
-#curl -H "Content-Type: application/json" localhost:9001/bulk/serviceLocations -d @serviceLocations.json
+#url_s=localhost:9001
+#url_f=localhost:9000
 
-curl -H "Content-Type: application/json" http://fleet-demo-fleet-location-service.cfapps.io/fleet -d @fleet.json
-curl -H "Content-Type: application/json" http://fleet-demo-service-location-service.cfapps.io/bulk/serviceLocations -d @serviceLocations.json
+url_s=http://fleet-demo-service-location-service.52.68.100.51.xip.io
+url_f=http://fleet-demo-fleet-location-service.52.68.100.51.xip.io
+
+echo ""
+echo "====="
+echo "Insert ServiceLocations"
+curl -XPOST -H "Content-Type: application/json" ${url_s}/purge
+curl -H "Content-Type: application/json" ${url_s}/bulk/serviceLocations -d @serviceLocations.json
+
+echo ""
+echo "====="
+echo "Insert Fleets"
+curl -XPOST -H "Content-Type: application/json" ${url_f}/purge
+curl -H "Content-Type: application/json" ${url_f}/fleet -d @fleet.json
+
+#curl -H "Content-Type: application/json" http://fleet-demo-service-location-service.cfapps.io/bulk/serviceLocations -d @serviceLocations.json
--- platform/configserver/cups.sh
+++ platform/configserver/cups.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cred='{"uri":"http://fleet-demo-config-server.cfapps.io"}'
+cred='{"uri":"http://fleet-demo-config-server.52.68.100.51.xip.io"}'
 cf create-user-provided-service fleet-demo-configserver -p $cred
