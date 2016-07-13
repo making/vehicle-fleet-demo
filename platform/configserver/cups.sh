18cce4e50a7f706b16032a1a280e2c83f7cd044a
making <makingx@gmail.com>
2016-07-13 10:55:45 +0900

    for PWS

--- platform/configserver/cups.sh
+++ platform/configserver/cups.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cred='{"uri":"http://fleet-demo-config-server.local.pcfdev.io"}'
+cred='{"uri":"http://fleet-demo-config-server.cfapps.io"}'
 cf create-user-provided-service fleet-demo-configserver -p $cred
--- platform/eureka/cups.sh
+++ platform/eureka/cups.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cred='{"uri":"http://fleet-demo-eureka-server.local.pcfdev.io"}'
+cred='{"uri":"http://fleet-demo-eureka-server.cfapps.io"}'
 cf create-user-provided-service fleet-demo-eureka -p $cred
