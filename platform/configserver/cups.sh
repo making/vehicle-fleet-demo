350cf6487c4e6df1ad7d5b46937cd3df6a9c07c1
making <makingx@gmail.com>
2016-08-22 14:49:29 +0900

    Fix url

--- platform/configserver/cups.sh
+++ platform/configserver/cups.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cred='{"uri":"http://fleet-demo-config-server.cfapps.io"}'
+cred='{"uri":"http://fleet-demo-config-server.local.pcfdev.io"}'
 cf create-user-provided-service fleet-demo-configserver -p $cred
